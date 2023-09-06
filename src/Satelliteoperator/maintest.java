81d32404c72fc9d7c79a3e4e6ef545282da8c590
kfzjw008 <1174681078@qq.com>
2023-09-07 07:50:11 +0800

    '代码自动同步2023-09-07'

--- src/Satelliteoperator/maintest.java
+++ src/Satelliteoperator/maintest.java
@@ -1,6 +1,7 @@
 package Satelliteoperator;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static Satelliteoperator.htnutil.*;
 
@@ -14,6 +15,7 @@ import static Satelliteoperator.htnutil.*;
 public class maintest {
     //规划模拟器
     private static String ADDRESS_FILE_MAKE="D:\\研究生\\学习\\研究内容\\【1】任务规划-HTN解决任务调度【主课题】\\202303\\源码\\JSHOP2_1.0.3\\JSHOP2\\bin\\PROBLEM";
+    static ArrayList<String> TaskLevelRepository = new ArrayList<>();//任务级指令存储
     public static void main(String[] args) throws IOException {
         //1首先基于任务级指令生成problem文件
         //2其次基于航天器现有资源生成domain文件
@@ -31,6 +33,10 @@ public class maintest {
         runCmd(ADDRESS_FILE_MAKE,"make");
 
         //4解析排序结果，执行时间线操作。
+
+
+
+
         //5执行资源冲突消解操作
         //6输出规划结果
     }
